e6368ef57145e51cd5c68b71df4fd586085798cc
Arda Akdemir <ardaakdemir1@gmail.com>
2021-11-26 22:57:22 +0900

    add steps per epoch

--- mlm_train_with_clregularization.sh
+++ mlm_train_with_clregularization.sh
@@ -7,8 +7,9 @@ experiment_root="../experiment_outputs/"
 save_folder=${1}
 eval_save_folder=${2}
 num_train_epochs=${3}
+steps_per_epoch=${4}
 #Train models
-singularity exec  --nv  --writable ~/singularity/dprk-image python3 mlm_trainer.py --mode train --with_cl_regularization --num_train_epochs ${num_train_epochs} --save_folder ${save_folder}
+singularity exec  --nv  --writable ~/singularity/dprk-image python3 mlm_trainer.py --mode train --with_cl_regularization --num_train_epochs ${num_train_epochs} --steps_per_epoch ${steps_per_epoch} --save_folder ${save_folder}
 
 model_path=${experiment_root}${save_folder}"/best_model_weights.pkh"
 #Eval
