4502be569e78d971943416072e805c2e0c8ff0d1
Arda Akdemir <ardaakdemir1@gmail.com>
2021-11-26 12:03:55 +0900

    turn off regularizer network

--- mlm_train_with_clregularization.sh
+++ mlm_train_with_clregularization.sh
@@ -1,6 +1,6 @@
 /usr/local/bin/nosh
 #$ -cwd
-#$ -l os7,v100=1,s_vmem=100G,mem_req=100G
+#$ -l v100=1,s_vmem=200G,mem_req=200G
 cd ~/dprk-research/DPRK-BERT
 
 experiment_root="../experiment_outputs/"
